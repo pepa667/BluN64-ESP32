353997f715f75082d5d5c77647fa030c03284a79
Pepa <pepa@pepa.dev.br>
2026-04-11 17:15:00 -0300

    dd

--- push.sh
+++ push.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
 read -p "Enter commit message: " msg
 git add .
 git commit -m "$msg" || :
 git push
-git submodule foreach "read -p 'Enter commit message for \"$name\": '  sub_msg && git add . && git commit -m \"$sub_msg\" || : && git push"
+
+# Push changes in all submodules
+git submodule foreach "read -p 'Enter commit message for \"$name\": ' sub_msg && git add . && git commit -m \"\$sub_msg\" || : && git push"
