2003cae0e70e478ba65ab4b578b7a089707f5f16
Pepa <pepa@pepa.dev.br>
2026-04-11 17:12:11 -0300

    Firmware

--- push.sh
+++ push.sh
@@ -3,4 +3,4 @@ read -p "Enter commit message: " msg
 git add .
 git commit -m "$msg" || :
 git push
-git submodule foreach "read -p 'Enter commit message for \${name}: '  sub_msg && git add . && git commit -m \"$sub_msg\" || : && git push"
+git submodule foreach "read -p 'Enter commit message for \"$name\": '  sub_msg && git add . && git commit -m \"$sub_msg\" || : && git push"
